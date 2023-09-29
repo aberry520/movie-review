9a8bcd5bbf7cc0901fb3e811ee0705299de1ab77
Austin Berry <aberry52098@gmail.com>
2023-09-29 15:45:47 -0400

    move styling outside of export to remedy warning in console

--- src/components/MovieResults.jsx
+++ src/components/MovieResults.jsx
@@ -1,35 +1,38 @@
 import styled from "styled-components"
-export const MovieResults = ({ search, imageClick }) => {
-    const MovieCard = styled.li`
-        /* background-color: blue; */
-        max-width: fit-content;
-        /* display: flex; */
+
+const MovieList = styled.div`
+        ul{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 10px;  
+        }
+        
+        li {
+            max-width: fit-content;
+        }
         label{
             display: flex;
             flex-direction: column;
         }
-       
     `
-    const MovieList = styled.ul`
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        gap: 10px;
-    `
+export const MovieResults = ({ search, imageClick }) => {
     return (
         <>
             <MovieList>
+                <ul>
                 {
                     search.map((item, index) =>
-                        
-                        <MovieCard key={index}>
-                                <label>{item.Title}
-                                    <br />
-                                    <img src={item.Poster} onClick={() => { imageClick(index) }} />
-                                </label>
-                        </MovieCard>
+
+                        <li key={index}>
+                            <label>{item.Title}
+                                <br />
+                                <img src={item.Poster} onClick={() => { imageClick(index) }} />
+                            </label>
+                        </li>
                     )
                 }
+                </ul>
             </MovieList>
         </>
     )
