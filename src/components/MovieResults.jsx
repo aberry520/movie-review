7de019951cdfd17a50fd376997e9403a6b8cc8a5
Austin Berry <aberry52098@gmail.com>
2023-09-29 15:34:22 -0400

    import styled-components and added styled elements

--- src/components/MovieResults.jsx
+++ src/components/MovieResults.jsx
@@ -1,14 +1,36 @@
-export const MovieResults = ({search, imageClick}) => {
+import styled from "styled-components"
+export const MovieResults = ({ search, imageClick }) => {
+    const MovieCard = styled.li`
+        /* background-color: blue; */
+        max-width: fit-content;
+        /* display: flex; */
+        label{
+            display: flex;
+            flex-direction: column;
+        }
+       
+    `
+    const MovieList = styled.ul`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 10px;
+    `
     return (
-        <><ul>
-        {
-            
-            search.map((item, index) =>
-            
-                <li key={index}><img src={item.Poster} onClick={() => { imageClick(index) }} /></li>
-            )
-            
-        }</ul>
+        <>
+            <MovieList>
+                {
+                    search.map((item, index) =>
+                        
+                        <MovieCard key={index}>
+                                <label>{item.Title}
+                                    <br />
+                                    <img src={item.Poster} onClick={() => { imageClick(index) }} />
+                                </label>
+                        </MovieCard>
+                    )
+                }
+            </MovieList>
         </>
     )
 }
