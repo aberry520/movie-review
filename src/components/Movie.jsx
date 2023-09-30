a72ab810dfc80969fd74f217e369a47321597bad
Austin Berry <aberry52098@gmail.com>
2023-09-30 10:43:14 -0400

    WORD_ARR, MovieHome -made word array that holds a list of words to search -made a random number function that picks a word from the word array to search -added all the logic to populate random movies for MovieHome

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,17 +1,19 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { MovieResults } from "./MovieResults"
 import { MovieSearch } from "./MovieSearch"
 import { MovieInfo } from "./MovieInfo"
 import { MovieHome } from "./MovieHome"
 
 export const Movie = () => {
-    const [searchInput, setSearchInput] = useState();
+    const [searchInput, setSearchInput] = useState("");
     const [poster, setPoster] = useState();
     const [title, setTitle] = useState();
     const [year, setYear] = useState();
     const [rating, setRating] = useState();
     const [search, setSearch] = useState([]);
+    const [plot, setPlot] = useState();
     const [dataError, setDataError] = useState();
+    const [homeDisplay, setHomeDisplay] = useState([]);
 
     const handleChange = (e) => {
         setSearchInput(e.target.value)
@@ -30,14 +32,68 @@ export const Movie = () => {
         }
     }
     const getSpecificData = async (index) => {
-        const apiUrl = `http://www.omdbapi.com/?t=${search[index].Title}&apikey=af468481`;
+        const apiUrl = `http://www.omdbapi.com/?i=${search[index].imdbID}&apikey=af468481&plot=full`;
         const response = await fetch(apiUrl);
         const data = await response.json();
-        setPoster(data.Poster);
+        if (data.Poster === "N/A"){
+            setPoster("../public/images/blank-poster.jpeg")
+            console.log("N/A")
+        } else {
+            setPoster(data.Poster);
+        }
+
         setTitle(data.Title);
         setYear(data.Released);
         setRating(data.Rated);
+        setPlot(data.Plot);
     };
+    const getSpecificHomeData = async (index) => {
+        const apiUrl = `http://www.omdbapi.com/?i=${homeDisplay[index].imdbID}&apikey=af468481&plot=full`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        if (data.Poster === "N/A"){
+            setPoster("../public/images/blank-poster.jpeg")
+            console.log("N/A")
+        } else {
+            setPoster(data.Poster);
+        }
+        
+        setTitle(data.Title);
+        setYear(data.Released);
+        setRating(data.Rated);
+        setPlot(data.Plot);
+    };
+    const randomID = (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+    const WORD_ARR = ["Love",
+        "Day",
+        "Night",
+        "Life",
+        "Time",
+        "Man",
+        "Woman",
+        "Story",
+        "World",
+        "Heart",
+        "Home",
+        "City",
+        "Game",
+        "Star",
+        "Island",
+        "Road",
+        "Secret",
+        "Adventure",
+        "Journey"]
+    const getHomeData = async () => {
+            const apiUrl = `http://www.omdbapi.com/?S=${WORD_ARR[randomID(0, 19)]}&apikey=af468481&page=${randomID(0,10)}&type=movie&y=${randomID(2009,2023)}`;
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+            setHomeDisplay(data.Search);
+            console.log(data.Search);
+    }
     const searchButton = () => {
         getSearchData();
         setSearchInput("");
@@ -45,17 +101,27 @@ export const Movie = () => {
         setTitle();
         setYear();
         setRating();
+        setPlot();
+        setHomeDisplay([]);
+        
     };
     const imageClick = (index) => {
         getSpecificData(index);
         setSearch([]);
     }
-    // console.log(search[3].Title);
+    const homeImageClick = (index) => {
+        getSpecificHomeData(index);
+        setHomeDisplay([]);
+    }    
     return (
         <>
-            <MovieSearch handleChange={handleChange} searchButton={searchButton} searchInput={searchInput} />
-            <MovieHome />
-            <MovieInfo title={title} rating={rating} year={year} poster={poster} />
+            <MovieSearch handleChange={handleChange} searchButton={searchButton} searchInput={searchInput} getHomeData={getHomeData}/>
+            <MovieHome homeDisplay={homeDisplay} homeImageClick={homeImageClick} getHomeData={getHomeData} searchInput={searchInput} />
+
+            {
+
+            }
+            <MovieInfo title={title} rating={rating} year={year} poster={poster} plot={plot}/>
             {
                 dataError === true &&
                 (<p>{search} Please refine search.</p>)
