920fbf10bf95d3875a59cd06364ca04bb7de4b19
Austin Berry <aberry52098@gmail.com>
2023-09-30 10:41:19 -0400

    display plot

--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -1,9 +1,10 @@
-export const MovieInfo = ({title,rating,year,poster}) => {
+export const MovieInfo = ({title,rating,year,poster,plot}) => {
     return (
         <>
             <h1>{title}</h1>
             <p>{rating}</p>
             <p>{year}</p>
+            <p>{plot}</p>
             <img src={poster} />
         </>
     )
