77f5d9569ae74e71cedbdfadc8e8882068186758
Austin Berry <aberry52098@gmail.com>
2023-09-30 10:55:29 -0400

    fix src links

--- src/components/MovieSearch.jsx
+++ src/components/MovieSearch.jsx
@@ -3,7 +3,7 @@ export const MovieSearch = ({handleChange, searchButton, searchInput, getHomeDat
     return (
         <>
             <form>
-                <img src="/images/movie-icon.png" style={{height:"40px"}} onClick={()=>{getHomeData()}}/>
+                <img src="./images/movie-icon.png" style={{height:"40px"}} onClick={()=>{getHomeData()}}/>
                 <input type="text" value={searchInput} onChange={(e) => { handleChange(e) }}/>
                 <button type="button" onClick={(e)=>{searchButton(e)}}>Search</button>
             </form>
