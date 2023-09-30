8d3940907d1d3db0048281955b37b8619d104c62
Austin Berry <aberry52098@gmail.com>
2023-09-30 10:37:48 -0400

    default movie poster logic, getHomeData logic
    -used useEffect to get data on page load for default home page to display random movies
    -if statement used to read data poster return to decide if default poster is needed

--- src/components/MovieHome.jsx
+++ src/components/MovieHome.jsx
@@ -1,7 +1,32 @@
-export const MovieHome = ({}) => {
+import { useEffect } from "react"
+
+export const MovieHome = ({homeDisplay, homeImageClick, getHomeData, searchInput}) => {
+    useEffect(()=>{
+        getHomeData()
+    },[])
+    const ifPoster = (item)=>{
+        if (item.Poster === "N/A"){
+            return "../public/images/blank-poster.jpeg"
+        } else {
+            return item.Poster
+        }
+    }
     return (
         <>
-            
+        
+            {
+                searchInput == "" &&
+                (homeDisplay.map((item, index) =>
+
+                    <li key={index}>
+                        <label>{item.Title}
+                            <br />
+                            <img src={ifPoster(item)} onClick={() => { homeImageClick(index) }} />
+                        </label>
+                    </li>
+                ))
+            }
+
         </>
     )
 }
